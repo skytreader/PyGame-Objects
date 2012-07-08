1a530e3c52dfe1354a4c92e66197150d240fed93
chad <chadestioco@gmail.com>
2012-07-08 16:13:19 +0800

    All tests in shapes_tests are now good.

--- shapes_tests.py
+++ shapes_tests.py
@@ -7,25 +7,23 @@ import unittest
 class shapes_tests(unittest.TestCase):
 	
 	def setUp(self):
-		self.__four_points = PointShape()
+		self.__four_points = PointShape([Point(3,4), Point(5,1), Point(8,10), Point(6,9)])
 		#self.__four_points.point_list = []
-		self.__four_points.add_point(Point(3, 4))
-		self.__four_points.add_point(Point(5, 1))
-		self.__four_points.add_point(Point(8, 10))
-		self.__four_points.add_point(Point(6, 9))
-		print "\n"
-		print self.__four_points.__str__()
+		#self.__four_points.add_point(Point(3, 4))
+		#self.__four_points.add_point(Point(5, 1))
+		#self.__four_points.add_point(Point(8, 10))
+		#self.__four_points.add_point(Point(6, 9))
 	
 	def test_translate(self):
-		#print ""
-		#print self.__four_points.__str__()
+		print ""
+		print self.__four_points.__str__()
 		self.__four_points.translate(1, 1)
 		translated = PointShape()
-		#print "Fresh translated:"
-		#print translated.__str__()
+		print "Fresh translated:"
+		print translated.__str__()
 		translated.point_list = [Point(4, 5), Point(6, 2), Point(9, 11), Point(7, 10)]
-		#print self.__four_points.__str__()
-		#print translated.__str__()
+		print self.__four_points.__str__()
+		print translated.__str__()
 		self.assertTrue(self.__four_points.__eq__(translated))
 	
 	def test___str__(self):
