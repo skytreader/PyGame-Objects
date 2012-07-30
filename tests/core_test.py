6e628d10f9e65d92bcef443cca5f8a604036215f
chad <chadestioco@gmail.com>
2012-07-30 16:57:36 +0800

    Removed event handling in core_test.py since it is now irrevelevant.

--- tests/core_test.py
+++ tests/core_test.py
@@ -1,8 +1,10 @@
 #! usr/bin/env python
 
+from ..components.core import GameConfig
 from ..components.core import GameLoopEvents
 from ..components.core import GameLoop
-from ..components.core import GameConfig
+from ..components.core import GameScreen
+
 import pygame
 import random
 
@@ -10,15 +12,13 @@ import random
 A simple demo of using the framework that randomly draws
 gray circles on a white canvas.
 
-Also demonstrates additional event handling.
-
 @author Chad Estioco
 """
 
 class GrayCircle(GameLoopEvents):
 	
 	def __init__(self, config):
-		super(GrayCircle, self).__init__(config)
+		super(GrayCircle, self).__init__(config, GameScreen([500,500]))
 		self.__limit = 100
 		self.__count = 0
 	
@@ -31,22 +31,10 @@ class GrayCircle(GameLoopEvents):
 		pygame.draw.circle(self.window, [218, 218, 218], [x, y], 50, 2)
 		self.__count += 1
 
-class GrayCircleLoop(GameLoop):
-	
-	def __init__(self, events):
-		super(GrayCircleLoop, self).__init__(events)
-	
-	def keydown(self, event):
-		print "Keydown pressed!"
-		print event.key
-	
-	def attach_event_handlers(self):
-		self.add_event_handler(pygame.event.Event(pygame.KEYDOWN), self.keydown)
-
 gconfig = GameConfig()
 gconfig.clock_rate = 10
 gconfig.window_size = [500, 500]
 gconfig.window_title = "Framework test"
 gc_object = GrayCircle(gconfig)
-game_loop = GrayCircleLoop(gc_object)
+game_loop = GameLoop(gc_object)
 game_loop.go()
