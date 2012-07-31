cffff7202a9d6b8f62ef214666ce5f59a9774a9c
chad <chadestioco@gmail.com>
2012-07-31 14:44:18 +0800

    Updated line_mesh_test to reflect framework changes.

--- tests/line_mesh_test.py
+++ tests/line_mesh_test.py
@@ -1,6 +1,6 @@
 #! usr/bin/env python
 
-from ..components.core import GameLoop, GameLoopEvents, GameConfig, Colors
+from ..components.core import GameLoop, GameLoopEvents, GameConfig, GameScreen, Colors
 from ..components.shapes import Point, PointShape
 
 import random
@@ -10,6 +10,21 @@ Demo stuff for PointShape.
 
 @author Chad Estioco
 """
+
+class LineMeshScreen(GameScreen):
+	
+	def __init__(self, screen_size):
+		super(LineMeshScreen, self).__init__(screen_size)
+	
+	@property
+	def triangle(self):
+		return self.__triangle
+	
+	def setup(self):
+		self.__triangle = PointShape([Point(250, 50), Point(50, 350), Point(400, 250)])
+	
+	def draw_screen(self, window):
+		self.triangle.draw(window)
 
 class LineMesh(GameLoopEvents):
 	"""
@@ -21,20 +36,19 @@ class LineMesh(GameLoopEvents):
 	I want a 500x500 window!
 	"""
 	
-	def __init__(self, config):
-		super(LineMesh, self).__init__(config)
+	def __init__(self, config, game_screen):
+		super(LineMesh, self).__init__(config, game_screen)
 		self.__limit = 200
 		self.__count = 0
 		
-		self.__triangle = PointShape([Point(250, 50), Point(50, 350), Point(400, 250)])
 	
 	def loop_invariant(self):
 		return self.__count < self.__limit
 	
 	def loop_event(self):
 		self.window.fill(Colors.WHITE)
-		self.__triangle.draw(self.window)
-		self.__triangle.add_point(Point(random.randint(0, 500), random.randint(0, 500)))
+		self.game_screen.triangle.draw(self.window)
+		self.game_screen.triangle.add_point(Point(random.randint(0, 500), random.randint(0, 500)))
 		
 		self.__count += 1
 
@@ -42,6 +56,9 @@ config = GameConfig()
 config.window_size = [500, 500]
 config.clock_rate = 10
 config.window_title = "Line Mesh"
-ms = LineMesh(config)
+
+game_screen = LineMeshScreen(config.window_size)
+
+ms = LineMesh(config, game_screen)
 game_loop = GameLoop(ms)
 game_loop.go()
