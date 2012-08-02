46810d0aa69c3f11a00db3a018b44f1f5b3c722f
chad <chadestioco@gmail.com>
2012-08-03 03:04:17 +0800

    Created my own sprite class to fit PyGame sprites into the framework. This class extends from both pygame.sprite.Sprite and subscriber_pattern.Observer.

---
+++ components/sprite.py
@@ -0,0 +1,44 @@
+#! usr/bin/env python
+
+from subscriber_pattern import Observer
+
+import pygame
+
+"""
+Convenience class to make PyGame Sprites fit into the
+scheme of the framework.
+
+@author Chad Estioco
+"""
+
+class Sprite(pygame.sprite.Sprite, Observer):
+	
+	def __init__(self, img):
+		"""
+		@param img
+		  An instance of Image, which is the sprite's character
+		  representation. Not to be confused with Sprite's self.image .
+		  This constructor automatically loads self.image with the surface
+		  contained in img .
+		"""
+		super(Sprite, self).__init__()
+		
+		self.__screen_draw = img
+		
+		self.image = img.img
+		self.rect = self.image.get_rect()
+		self.rect.y = img.position.y
+		self.rect.x = img.position.x
+	
+	@property
+	def screen_draw(self):
+		return self.__screen_draw
+	
+	@screen_draw.setter
+	def screen_draw(self, i):
+		self.__screen_draw = i
+	
+	def notify(self, observed, arg_bundle = None):
+		self.image = self.screen_draw.img
+		self.rect.x = self.screen_draw.position.x
+		self.rect.y = self.screen_draw.position.y
