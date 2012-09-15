c9c5b3699ba8d921110d180e7e6bb368e9a6adde
skytreader <chadestioco@gmail.com>
2012-09-15 23:43:51 +0800

    Was away today So all I will show for now Are import code lines :|

---
+++ tests/trees/tree.py
@@ -0,0 +1,10 @@
+#! usr/bin/env python
+
+from ...components.core import Colors
+from ...components.core import GameConfig
+from ...components.core import GameLoop
+from ...components.core import GameLoopEvents
+from ...components.core import GameScreen
+
+from ...components.shapes import Point
+from ...components.shapes import PointShape
