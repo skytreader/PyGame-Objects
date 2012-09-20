995ef544119f6a3f9ec881f60892ba2fc6865227
skytreader <chadestioco@gmail.com>
2012-09-20 22:27:27 +0800

    Added game model In color_blocks_game.py. Let's get this rolling!

--- tests/color_blocks/color_blocks_game.py
+++ tests/color_blocks/color_blocks_game.py
@@ -4,7 +4,26 @@ from ...components.core import Colors
 from ...components.core import GameConfig
 from ...components.core import GameScreen
 
+from color_blocks_model import ColorBlocksModel
+
 class ColorBlocksScreen(GameScreen):
 	
-	def __init__(self, dimensions):
+	def __init__(self, dimensions, grid_size):
+		"""
+		Instantiates a ColorBlocksScreen instance.
+		
+		@param dimensions
+		  The dimensions of the screen. An iterable whose first element
+		  is taken for the width while the second one is taken for the
+		  height.
+		@param grid_size
+		  The size of the grid, in squares per dimension. First element
+		  is taken for the width while the second one is taken for the
+		  height.
+		"""
 		super(GameScreen, self).__init__(dimensions)
+		self.__game_model = ColorBlocksModel(grid_size[0], grid_size[1])
+	
+	@property
+	def game_model(self):
+		return self.__game_model
