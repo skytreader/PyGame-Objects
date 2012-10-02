e33b962d1ee7f850697a8f19a16ad7dadb1894ca
skytreader <chadestioco@gmail.com>
2012-10-02 23:42:24 +0800

    Now I'm scheming for Common UI elements. Must. Start. Planning. This.

---
+++ components/common_ui.py
@@ -0,0 +1,19 @@
+from core import Colors
+
+from shapes import Point
+from shapes import PointShape
+
+"""
+This module manages some common-UI functionalities like buttons
+and menus.
+
+TODO: Maybe, integrate this with common_shapes ?
+"""
+
+class Button(object):
+	"""
+	A button has three states: untouched, hover, and pushed.
+	"""
+	
+	def __init__(self, upper_left, lower_right):
+		pass
