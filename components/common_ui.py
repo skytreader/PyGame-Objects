ec0c0317c62652854135ee343e9e1f9d05137375
skytreader <chadestioco@gmail.com>
2012-10-04 23:34:27 +0800

    Worked on UI code Some properties for buttons... Ah! Busy playing!

--- components/common_ui.py
+++ components/common_ui.py
@@ -1,4 +1,10 @@
+#! usr/bin/env python
+
+from common_shapes import Rectangle
+
 from core import Colors
+
+from drawable import Drawable
 
 from shapes import Point
 from shapes import PointShape
@@ -8,12 +14,32 @@ This module manages some common-UI functionalities like buttons
 and menus.
 
 TODO: Maybe, integrate this with common_shapes ?
+TODO: If we are to use common shapes with this, we must have a facility
+to flood fill PointShapes.
 """
 
-class Button(object):
+class Button(Drawable):
 	"""
 	A button has three states: untouched, hover, and pushed.
 	"""
 	
-	def __init__(self, upper_left, lower_right):
-		pass
+	def __init__(self, upper_left, lower_right, btn_color = Colors.LUCID_DARK, text, text_color = Colors.WHITE):
+		self.__button_area = Rectangle(upper_left, lower_right)
+		self.__button_text = text
+		self.__text_color = text_color
+	
+	@property
+	def button_area(self):
+		return self.__button_area
+	
+	@property
+	def button_text(self):
+		return self.__button_text
+	
+	@button_text.setter
+	def button_text(self, new_text):
+		self.__button_text = new_text
+	
+	@property
+	def text_color(self):
+		return self.__text_color
