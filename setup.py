08b3a0bf1ee07fd365d7d4e0227b2fa2fc3d1b3e
Chad Estioco <chadestioco@gmail.com>
2016-02-28 04:47:22 +0800

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name="PyGame Objects",
+    version="0.1.0",
+    author="Chad Estioco",
+    author_email="chadestioco@gmail.com",
+    url="https://github.com/skytreader/PyGame-Objects",
+    packages=["pygame-objects"],
+    install_requires=["pygame"],
+    license="MIT",
+    description="PyGame Framework"
+)
