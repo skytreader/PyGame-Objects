8af7425dc79251d127c9b880a0f15e92635d8bc5
Chad Estioco <chadestioco@gmail.com>
2016-07-23 06:16:50 +0800

    Again I'm not sure if I'm using Python's features properly.

--- demo/snake/model.py
+++ demo/snake/model.py
@@ -4,6 +4,17 @@ class Snake(object):
     
     def __init__(self, size):
         self.size = size
+        """
+        Sorted (row, col) tuples of the snake joints. Notice that joints can
+        only be made from the snake's head and can only disappear from the
+        snake's tail. The current location of the head is not considered a joint
+        but the tail is.
+
+        At any given time, the length of this list cannot be greater than the
+        size of the snake.
+        """
+        self.joints = []
+        self.head = None
 
 class GameModel(object):
     
@@ -15,24 +26,20 @@ class GameModel(object):
 
         self.grid_size = [[False for _ in range(width)] for __ in range(height)]
         self.snake = Snake(GameModel.DEFAULT_SNAKE_SIZE)
-        """
-        Sorted (row, col) tuples of the snake joints. Notice that joints can
-        only be made from the snake's head and can only disappear from the
-        snake's tail. The current location of the head is not considered a joint
-        but the tail is.
 
-        At any given time, the length of this list cannot be greater than the
-        size of the snake.
-        """
-        self.snake_joints = []
-        # The location of the snake's head.
-        self.snake_head = None
+    @property
+    def snake_joints(self):
+        return self.snake.joints
+
+    @property
+    def snake_head(self):
+        return self.snake.head
 
     def initialize(self):
         # Relies on Python 2.x division behavior.
         row = len(self.grid_size[0]) / 2
         col = len(self.grid_size[1]) / 2
-        self.snake_head = (row, col)
+        self.snake.head = (row, col)
         self.snake_joints.append((row, col - self.snake.size))
 
     def move_snake(self, direction):
@@ -49,7 +56,7 @@ class GameModel(object):
             return
 
         self.snake_joints.insert(0, self.snake_head)
-        self.snake_head = (self.snake_head[0] + movector[0], self.snake_head[1] + movector[1])
+        self.snake.head = (self.snake_head[0] + movector[0], self.snake_head[1] + movector[1])
 
         snake_tail_vector = QuadraticGrid.Movements.compute_direction(
           self.snake_joints[-1], self.snake_joints[-2]
