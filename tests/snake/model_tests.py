f28c7ed0aa82e9e7eb83c7430b12ed131ed4a560
Chad Estioco <chadestioco@gmail.com>
2016-08-06 05:56:07 +0800

    Make tests pass.

--- tests/snake/model_tests.py
+++ tests/snake/model_tests.py
@@ -1,11 +1,11 @@
 import unittest
 
-from demo.snake.model import GameModel, Snake
+from demo.snake.model import SnakeGameModel, Snake
 
 class SnakeModelTests(unittest.TestCase):
     
     def setUp(self):
-        self.gm = GameModel(10, 10)
+        self.gm = SnakeGameModel(10, 10)
         self.gm.initialize()
 
     def test_init(self):
@@ -17,15 +17,15 @@ class SnakeModelTests(unittest.TestCase):
         self.assertTrue(self.gm.snake_head is not None)
     
     def test_constructor_exception(self):
-        self.assertRaises(ValueError, GameModel, 0, 0)
-        self.assertRaises(ValueError, GameModel, GameModel.DEFAULT_SNAKE_SIZE,
-          GameModel.DEFAULT_SNAKE_SIZE)
-        less1 = GameModel.DEFAULT_SNAKE_SIZE
-        self.assertRaises(ValueError, GameModel, less1, less1)
-        self.assertRaises(ValueError, GameModel, -1, -1)
+        self.assertRaises(ValueError, SnakeGameModel, 0, 0)
+        self.assertRaises(ValueError, SnakeGameModel, SnakeGameModel.DEFAULT_SNAKE_SIZE,
+          SnakeGameModel.DEFAULT_SNAKE_SIZE)
+        less1 = SnakeGameModel.DEFAULT_SNAKE_SIZE - 1
+        self.assertRaises(ValueError, SnakeGameModel, less1, less1)
+        self.assertRaises(ValueError, SnakeGameModel, -1, -1)
 
     def test_constructor_happy(self):
-        gm = GameModel(4, 4)
+        gm = SnakeGameModel(4, 4)
         self.assertTrue(gm.snake is not None)
 
     def test_move_snake_exceptions(self):
@@ -77,7 +77,7 @@ class SnakeModelTests(unittest.TestCase):
 
     def test_bending(self):
         snake_head = self.gm.snake_head
-        max_len = GameModel.DEFAULT_SNAKE_SIZE
+        max_len = SnakeGameModel.DEFAULT_SNAKE_SIZE
         old_len = len(self.gm.snake_joints)
         old_tail = self.gm.snake_joints[-1]
         self.assertTrue(max_len >= old_len)
