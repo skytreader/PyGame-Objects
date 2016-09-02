26a7a48bae253d4ec8460f271b811d23b1f14131
Chad Estioco <chadestioco@gmail.com>
2016-09-03 04:44:36 +0800

    Add logs when a wall collision occurs. But the snake stil moves through!

--- demo/snake/game.py
+++ demo/snake/game.py
@@ -50,11 +50,18 @@ class SnakeGameEvents(GameLoopEvents):
     def __create_move_event_handler(self, key):
         def event_handler(event):
             height = self.game_screen.game_model.height
+            width = self.game_screen.game_model.width
             movement = SnakeGameEvents.PYGAME_TO_MOVE[key]
             self.debug_queue.log(str(movement))
             self.debug_queue.log("grid height is %s" % height)
             self.game_screen.model.move_snake(movement)
             self.debug_queue.log("snake head now at %s" % str(self.game_screen.model.snake.head))
+            new_head = self.game_screen.model.snake.head
+
+            if new_head[0] >= height or new_head[1] >= width or new_head[0] < 0 or new_head[1] < 0:
+                inverse = QuadraticGrid.Movements.INVERSE_DIRECTION[movement]
+                self.game_screen.model.move_snake(inverse)
+                self.debug_queue.log("WALL COLLISION")
         
         return event_handler
 
