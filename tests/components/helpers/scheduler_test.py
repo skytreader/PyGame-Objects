0f73bbb03b62586e1ace3eb127e3fd7d2b858742
Chad Estioco <chadestioco@gmail.com>
2016-09-27 22:26:28 +0800

    Add tests for our poor-man's scheduler.

---
+++ tests/components/helpers/scheduler_test.py
@@ -0,0 +1,34 @@
+from components.helpers.scheduler import Scheduler
+from mock import MagicMock, Mock, patch
+
+import unittest
+
+class Event(object):
+    
+    def __init__(self):
+        self.times_called = 0
+
+    def happen(self):
+        self.times_called = 3
+
+class SchedulerTests(unittest.TestCase):
+    
+    MOCK_FRAME_RATE = 18
+    MOCK_EVENT_FREQUENCY = 3
+    
+    def setUp(self):
+        self.event = Event()
+        self.scheduler = Scheduler(SchedulerTests.MOCK_FRAME_RATE,
+          SchedulerTests.MOCK_EVENT_FREQUENCY, self.event.happen)
+
+    def test_event(self):
+        for _ in range(SchedulerTests.MOCK_FRAME_RATE):
+            self.scheduler.event()
+
+        self.assertEquals(Scheduler.MOCK_EVENT_FREQUENCY, self.event.times_called)
+
+        for _ in range(SchedulerTests.MOCK_FRAME_RATE):
+            self.scheduler.event()
+
+        self.assertEquals(SchedulerTests.MOCK_EVENT_FREQUENCY * 2,
+          self.event.times_called)
