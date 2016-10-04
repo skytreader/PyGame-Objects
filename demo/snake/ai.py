c0ea16701321a9513aafcc8520dc84e00e2c2b85
Chad Estioco <chadestioco@gmail.com>
2016-10-04 23:11:03 +0800

    More sketch.

--- demo/snake/ai.py
+++ demo/snake/ai.py
@@ -1,14 +1,23 @@
 from components.helpers.grid import QuadraticGrid
 
+import heapq
 import time
 
 class WindowedCount(object):
     
     def __init__(self, window=8):
+        self.window_size = window
         self.window_counts = {}
-        self.timestamps = {}
+        self.record_size = 0
+        self.timeheap = []
 
     def incr(self, count_key):
+        assert self.record_size <= self.window_size
+        if self.record_size == self.window_size:
+            oldest_entry = heapq.heappop(self.timeheap)
+            self.window_counts[oldest_entry] -= 1
+            self.record_size -= 1
+
         count = self.window_counts.get(count_key)
         
         if count:
@@ -16,10 +25,23 @@ class WindowedCount(object):
         else:
             self.window_counts[count_key] = 1
 
-        self.timestamps[count_key] = int(time.time() * 1000)
+        heapq.heappush(self.timeheap, (time.time(), count_key))
 
 class SpawnManager(object):
     
     def __init__(self, window=8):
         self.global_counts = {}
         self.window_counts = WindowedCount()
+
+    def note_movement(self, movement):
+        self.window_counts.incr(movement)
+
+        count = self.global_counts.get(movement)
+
+        if count:
+            self.global_counts[count_key] += 1
+        else:
+            self.global_counts[count_key] = 1
+
+    def get_spawn(self):
+        pass
