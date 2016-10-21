e16668d81983412cd406ada49e661cb90c3d4748
Chad Estioco <chadestioco@gmail.com>
2016-10-22 05:53:58 +0800

    Document exceptions.

--- components/framework_exceptions.py
+++ components/framework_exceptions.py
@@ -1,15 +1,20 @@
 #! usr/bin/env python
 
-class InstanceException(Exception):
-    
-    def __init__(self, value):
-        self.value = value
-    
-    def __str__(self):
-        return repr(self.value)
-
 class VectorDirectionException(Exception):
+    """
+    Used by methods taking in a vector with the assumption that the vector
+    describes a particular direction movement.
+    """
     pass
 
 class InvalidConfigStateException(ValueError):
+    """
+    Used to signal invalid configuration values.
+    """
     pass
+
+class MalformedDialogException(Exception):
+    """
+    Raised when the dialog file is in improper form. The message should specify
+    exactly how/why the dialog file is invalid.
+    """
