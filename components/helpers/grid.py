4c4f4b9f4d32fca8ccd981d44d551ef5e68fc371
Chad Estioco <chadestioco@gmail.com>
2018-02-25 07:35:05 +0800

    Damn typo.

--- components/helpers/grid.py
+++ components/helpers/grid.py
@@ -1,7 +1,7 @@
 #! usr/bin/env python
 from __future__ import division
 
-from components.core import Color
+from components.core import Colors
 from components.drawable import Drawable
 from components.framework_exceptions import VectorDirectionException
 
