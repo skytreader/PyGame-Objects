ac1b82bb896978f13bf4000abf8640890af2afc8
Chad Estioco <chadestioco@gmail.com>
2018-02-25 06:13:50 +0800

    Update the version in setup.py with the actual stated version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="PyGame Objects",
-    version="0.1.0",
+    version="1.0.2-dev",
     author="Chad Estioco",
     author_email="chadestioco@gmail.com",
     url="https://github.com/skytreader/PyGame-Objects",
