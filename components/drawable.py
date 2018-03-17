3a780e62971e06b1d162434523d313522c6afa21
Chad Estioco <chadestioco@gmail.com>
2018-03-17 20:40:29 +0800

    Add a bare-bones drawing method for Button.

--- components/drawable.py
+++ components/drawable.py
@@ -15,6 +15,7 @@ class Drawable(object):
         draw_offset is a (width, height) tuple.
         """
         self.draw_offset = draw_offset if draw_offset else (0, 0)
+        self.max_size = (width_limit, height_limit)
     
     def draw(self, window, screen, **kwargs):
         """
