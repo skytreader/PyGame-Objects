39ef1a5c65d63609cefcbc2773f925f5b5dcd828
Chad Estioco <chadestioco@gmail.com>
2018-04-02 01:57:39 +0800

    RELEASE VERSION BUMP.
    The magnitude of this version bump is incompatible with the magnitude of changes
    contained in this release.
    
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def get_font_paths():
 
 setup(
     name="PyGame Objects",
-    version="1.1.1-dev",
+    version="1.2.0",
     author="Chad Estioco",
     author_email="chadestioco@gmail.com",
     url="https://github.com/skytreader/PyGame-Objects",
